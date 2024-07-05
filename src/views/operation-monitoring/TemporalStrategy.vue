cf3caaa33fd4fa5de96c8ce86abeda9d6317f891
tang jun <justdoitang@163.com>
2024-07-05 17:00:22 +0800

    feat：TemporalStrategy

--- src/views/operation-monitoring/TemporalStrategy.vue
+++ src/views/operation-monitoring/TemporalStrategy.vue
@@ -19,32 +19,62 @@ const options = [
   }
 ]
 
-let isIndeterminate = false
-let checkAll = false
+//let isIndeterminate = false
 const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 const weeks = ["日", "一", "二", "三", "四", "五", "六"]
 const days = Array.from({ length: 31 }, (_, i) => i + 1)
 const hours = Array.from({ length: 24 }, (_, i) => i)
 const minutes = Array.from({ length: 60 }, (_, i) => i)
 
-const handleCheckAllChange = (val: any) => {
+const handleSelectMonthChange = (val: any) => {
   formData.value.months = val ? months : []
-  isIndeterminate = false
 }
-
 const handleCheckedMonthsChange = (val: any) => {
   const checkedCount = val.length
-  checkAll = checkedCount === formData.value.months.length
-  isIndeterminate = checkedCount > 0 && checkedCount < formData.value.months.length
+  formData.value.isMonths = checkedCount > 0
+}
+const handleSelectWeekChange = (val: any) => {
+  console.log(val)
+  formData.value.weeks = val ? weeks : []
+}
+const handleCheckedWeekChange = (val: any) => {
+  const checkedCount = val.length
+  formData.value.isWeek = checkedCount > 0
+}
+const handleSelectDayChange = (val: any) => {
+  formData.value.days = val ? days : []
+}
+const handleCheckedDayChange = (val: any) => {
+  const checkedCount = val.length
+  formData.value.isDay = checkedCount > 0
+}
+const handleSelectHourChange = (val: any) => {
+  formData.value.hours = val ? hours : []
+}
+const handleCheckedHourChange = (val: any) => {
+  const checkedCount = val.length
+  formData.value.isHour = checkedCount > 0
+}
+const handleSelectMinuteChange = (val: any) => {
+  formData.value.minutes = val ? minutes : []
+}
+const handleCheckedMinuteChange = (val: any) => {
+  const checkedCount = val.length
+  formData.value.isMinute = checkedCount > 0
 }
 
 const DEFAULT_FORM_DATA: EnergyParamBaseData = {
   Id: undefined,
   Name: "",
+  isMonths: false,
   months: [],
+  isWeek: false,
   weeks: [],
+  isDay: false,
   days: [],
+  isHour: false,
   hours: [],
+  isMinute: false,
   minutes: [],
   frequency: "",
   maxDelay: "",
@@ -61,8 +91,8 @@ const formData = ref<EnergyParamBaseData>(cloneDeep(DEFAULT_FORM_DATA))
       </el-card>
     </div>
     <div class="right-card">
-      <el-card class="contrast-card">
-        <el-form class="form-style" :model="formData" :label-width="140" label-position="left">
+      <el-card>
+        <el-form class="form-style" :model="formData" label-width="130px" label-position="left">
           <div class="form-col">
             <el-col :span="12"
               ><el-form-item label="任务编号：" class="form-item-label">
@@ -100,62 +130,76 @@ const formData = ref<EnergyParamBaseData>(cloneDeep(DEFAULT_FORM_DATA))
                 </el-select> </el-form-item
             ></el-col>
           </div>
-          <div class="checkbox-group-all">
-            <el-form-item label="按月执行：" class="form-item-label">
-              <el-checkbox :indeterminate="isIndeterminate" v-model="formData.Name" @change="handleCheckAllChange" />
+          <div>
+            <el-form-item label="按月执行：" class="form-item-label el-form-item1">
+              <el-checkbox v-model="formData.isMonths" @change="handleSelectMonthChange" />
             </el-form-item>
           </div>
           <div class="checkbox-group">
             <el-checkbox-group v-model="formData.months" @change="handleCheckedMonthsChange">
-              <el-checkbox v-for="month in months" :label="month" :key="month">{{ month }}月</el-checkbox>
+              <el-checkbox v-for="month in months" :value="month" :key="month">{{ month }}月</el-checkbox>
             </el-checkbox-group>
           </div>
-          <div class="checkbox-group-all">
-            <el-form-item label="按周执行：" class="form-item-label">
-              <el-checkbox :indeterminate="isIndeterminate" v-model="formData.Name" @change="handleCheckAllChange" />
+          <div>
+            <el-form-item label="按周执行：" class="form-item-label el-form-item1">
+              <el-checkbox v-model="formData.isWeek" @change="handleSelectWeekChange" />
             </el-form-item>
           </div>
           <div class="checkbox-group">
-            <el-checkbox-group v-model="formData.weeks" @change="handleCheckedMonthsChange">
-              <el-checkbox v-for="week in weeks" :label="week" :key="week">周{{ week }}</el-checkbox>
+            <el-checkbox-group v-model="formData.weeks" @change="handleCheckedWeekChange">
+              <el-checkbox v-for="week in weeks" :value="week" :key="week">周{{ week }}</el-checkbox>
             </el-checkbox-group>
           </div>
-          <div class="checkbox-group-all">
-            <el-form-item label="按天执行：" class="form-item-label">
-              <el-checkbox :indeterminate="isIndeterminate" v-model="formData.Name" @change="handleCheckAllChange" />
+          <div>
+            <el-form-item label="按天执行：" class="form-item-label el-form-item1">
+              <el-checkbox v-model="formData.isDay" @change="handleSelectDayChange" />
             </el-form-item>
           </div>
           <div class="checkbox-group">
-            <el-checkbox-group v-model="formData.days" @change="handleCheckedMonthsChange">
-              <el-checkbox v-for="day in days" :label="day" :key="day">{{ day }}号</el-checkbox>
+            <el-checkbox-group v-model="formData.days" @change="handleCheckedDayChange">
+              <el-checkbox v-for="day in days" :value="day" :key="day">{{ day }}号</el-checkbox>
             </el-checkbox-group>
           </div>
-          <div class="checkbox-group-all">
-            <el-form-item label="按小时执行：" class="form-item-label">
-              <el-checkbox :indeterminate="isIndeterminate" v-model="formData.Name" @change="handleCheckAllChange" />
+          <div>
+            <el-form-item label="按小时执行：" class="form-item-label el-form-item1">
+              <el-checkbox v-model="formData.isHour" @change="handleSelectHourChange" />
             </el-form-item>
           </div>
           <div class="checkbox-group">
-            <el-checkbox-group v-model="formData.hours" @change="handleCheckedMonthsChange">
-              <el-checkbox v-for="hour in hours" :label="hour" :key="hour">{{ hour }}</el-checkbox>
+            <el-checkbox-group v-model="formData.hours" @change="handleCheckedHourChange">
+              <el-checkbox v-for="hour in hours" :value="hour" :key="hour">{{ hour }}</el-checkbox>
             </el-checkbox-group>
           </div>
-          <div class="checkbox-group-all">
-            <el-form-item label="按分钟执行：" class="form-item-label">
-              <el-checkbox :indeterminate="isIndeterminate" v-model="formData.Name" @change="handleCheckAllChange" />
+          <div>
+            <el-form-item label="按分钟执行：" class="form-item-label el-form-item1">
+              <el-checkbox v-model="formData.isMinute" @change="handleSelectMinuteChange" />
             </el-form-item>
           </div>
           <div class="checkbox-group">
-            <el-checkbox-group v-model="formData.minutes" @change="handleCheckedMonthsChange">
-              <el-checkbox v-for="minute in minutes" :label="minute" :key="minute">{{ minute }}</el-checkbox>
+            <el-checkbox-group v-model="formData.minutes" @change="handleCheckedMinuteChange">
+              <el-checkbox v-for="minute in minutes" :value="minute" :key="minute">{{ minute }}</el-checkbox>
             </el-checkbox-group>
+          </div>
+          <div class="input-group">
+            <el-form-item label="频率(分)：" class="form-item-label">
+              <el-input v-model="formData.Name" />
+              <label class="input-group-label">【最小执行频率(分)，0代表数据上传时即时解发】</label>
+            </el-form-item>
+          </div>
+          <div class="input-group">
+            <el-form-item label="表达式：" class="form-item-label">
+              <el-input v-model="formData.Name" />
+              <label class="input-group-label">【要满足的表达式（如果总是执行,请填入1）】</label>
+            </el-form-item>
+          </div>
+          <div class="input-group">
+            <el-form-item label="设定值表达式：" class="form-item-label">
+              <el-input v-model="formData.Name" />
+              <label class="input-group-label">【当满足表达式时，设置的被控制动作的值的表达式】</label>
+            </el-form-item>
           </div>
         </el-form>
-        <el-col :span="12"
-          ><el-form-item label="任务编号：" class="form-item-label">
-            <el-input v-model="formData.Name" /> <label>dada</label></el-form-item
-          ></el-col
-        >
+        <div style="text-align: center"><el-button type="primary" :icon="CirclePlus">保存</el-button></div>
       </el-card>
     </div>
   </div>
@@ -172,6 +216,9 @@ const formData = ref<EnergyParamBaseData>(cloneDeep(DEFAULT_FORM_DATA))
     font-weight: bold;
   }
 }
+.el-card {
+  height: 920px;
+}
 .left-card {
   flex: 2;
   margin-right: 15px;
@@ -186,9 +233,13 @@ const formData = ref<EnergyParamBaseData>(cloneDeep(DEFAULT_FORM_DATA))
 }
 .form-style {
   .form-item-label {
-    font-size: 20px;
     font-weight: bolder;
   }
+}
+.el-form-item {
+  display: flex;
+  --font-size: 14px;
+  margin-bottom: 12px;
 }
 .form-style .el-input {
   --el-input-width: 300px;
@@ -196,6 +247,7 @@ const formData = ref<EnergyParamBaseData>(cloneDeep(DEFAULT_FORM_DATA))
 .checkbox-group {
   border: 3px solid #000;
   align-items: center;
+  margin-bottom: 8px;
   .el-checkbox-group {
     align-items: center;
     margin-left: 15px;
@@ -204,6 +256,25 @@ const formData = ref<EnergyParamBaseData>(cloneDeep(DEFAULT_FORM_DATA))
   }
   .el-checkbox {
     margin-left: 30px;
+    width: 40px;
+  }
+}
+.el-form-item1 {
+  margin-bottom: 0px;
+}
+.input-group {
+  margin-top: 20px;
+  margin-bottom: -10px;
+  .el-input {
+    width: 160px;
+  }
+  .input-group-label {
+    margin-left: 10px;
+    color: red;
+  }
+  .el-form-item__label {
+    width: 100px; /* 你可以根据需要调整宽度 */
+    display: inline-block; /* 确保 label 元素是块级元素 */
   }
 }
 </style>
